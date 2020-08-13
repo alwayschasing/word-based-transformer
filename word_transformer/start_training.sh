53115a1daeb5adbd2a6274b9c1b8546a65c0a117
liruihong <liruihongbupt@163.com>
2020-08-13 14:48:41 +0800

    commit change

--- word_transformer/start_training.sh
+++ word_transformer/start_training.sh
@@ -3,18 +3,22 @@ export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:$LD_LIBRARY_PATH
 export PATH=/usr/local/cuda-10.0/bin:$PATH
 export CUDA_HOME=/usr/local/cuda-10.0:$CUDA_HOME
 
+
+config_file="/search/odin/liruihong/word-based-transformer/config_data/model_config.json"
+vocab_file="/search/odin/liruihong/word2vec_embedding/2000000-small.txt"
+input_file=""
+cached_tfrecord=""
+output_dir=""
+
 python keyword_extract.py \
-    --config_file="/search/odin/liruihong/NLPTool/config_data/model_config.json" \
-    --vocab_file="/search/odin/liruihong/word2vec_embedding/2000000-small.txt" \
-    --stop_words_file="/search/odin/liruihong/word2vec_embedding/cn_stopwords.txt" \
-    --embedding_table="/search/odin/liruihong/word2vec_embedding/2000000-small.txt" \
-    --use_pos=True \
-    --output_dir="/search/odin/liruihong/NLPTool/model_output/article2d_small_epoch2_test" \
-    --embedding_table_trainable=False \
-    --embedding_size=200 \
+    --config_file=$config_file \
+    --vocab_file=$vocab_file \
+    --output_dir=$output_dir \
+    --input_file=""
+    --embedding_table_trainable=True \
+    --embedding_size=128 \
     --max_seq_length=256 \
     --save_checkpoint_steps=2000 \
     --do_train=True \
-    --train_data="/search/odin/liruihong/NLPTool/datasets/article2d_pos.tfrecord" \
     --num_warmup_steps=1000 \
-    --num_train_steps=20000
+    --num_train_epochs=3
